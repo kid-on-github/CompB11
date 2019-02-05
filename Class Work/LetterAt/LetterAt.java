de7c6af1329f297c412bcf1ed032768a203d500b
Mike <mike@lemonshell.com>
2019-02-05 14:06:38 -0800

    Add classwork

---
+++ Class Work/LetterAt/LetterAt.java
@@ -0,0 +1,26 @@
+
+/* 
+* This program asks for word and states if the word begins with the letter a. 
+*
+* @author Alejandra Zapata and Michael Peterson
+* Course: COMP B11
+* Created: Jan 31, 2019
+* Source File: IfAndString.java
+*/
+
+import java.util.Scanner;
+
+public class LetterAt {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter word: ");
+		String inWord = in.next();
+		if (inWord.substring(0,1).equals("a")) {
+			System.out.printf("\"%s\" starts with 'a'.\n",inWord);
+		}
+		else {  
+			System.out.printf("\"%s\" does not start with 'a'.\n",inWord);
+		}
+	}
+}
---
+++ Class Work/try/Try.java
@@ -0,0 +1,21 @@
+
+/* 
+* This program asks for word and states if the word begins with the letter a. 
+*
+* @author Alejandra Zapata and Michael Peterson
+* Course: COMP B11
+* Created: Jan 31, 2019
+* Source File: IfAndString.java
+*/
+
+
+public class Try {
+	public static void main(String[] args) {
+
+		System.out.println("abc".compareTo("ABC"));
+		System.out.println("ABC".compareTo("abc"));
+		System.out.println("abC".compareTo("abc"));
+		
+		
+	}
+}
