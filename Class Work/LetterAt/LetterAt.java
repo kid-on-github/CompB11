00694da16084d4413b44181d1776dfdc1d4e8b52
Mike <mike@lemonshell.com>
2019-02-05 14:47:58 -0800

    update

--- Class Work/LetterAt/LetterAt.java
+++ Class Work/LetterAt/LetterAt.java
@@ -1,22 +1,11 @@
 
 /* 
-* This program asks for word and states if the word begins with the letter a. 
+* This program will display the desired index of word entered 
 *
 * @author Michael Peterson and Alejandra Zapata
 * Course: COMP B11
 * Created: Feb 5, 2019
 * Source File: IfAndString.java
-
- prompt the user to enter a word
- read the word
- prompt the user to enter an index into the word
- read the index (You can assume the user enters a non-negative integer)
- 
- if the index is legal, print out the letter at the given index, in the format
- shown in the examples below, using printf properly. 
- 
- if the index is NOT legal, print out an error message in the format shown in
- the examples below.
 */
 
 import java.util.Scanner;
